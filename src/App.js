f1fee1a05c703db80ee1791b0714129518b593b2
annu singh chauhan <annusingh2403@gmail.com>
2022-09-02 13:29:56 +0530

    image added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import logo from './logo.svg';
+import image from './images/image-product-desktop.jpg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hi annu singh</h1>
+    <div className='body'>
+        <div className='main'>
+            <div className='img-box'>
+                <img src={image} alt=""/>
+            </div>
+            <div className='info-box'>
+                <p>
+                    Parfume
+                </p>
+                <h1>
+                    Gabrielle Essence Eau De Parfume
+                </h1>
+                <p>
+                    A floral, solar and voluptuous interpretation composed by Olivier Polge, 
+                    Perfumer-Creator for the House of CHANEL.
+                </p>
+                <div>
+                    <h4>
+                        $149.99
+                    </h4>
+                    <p>
+                        $169.99
+                    </p>
+                </div>
+                <button>Add To Cart</button>
+            </div>
+        </div>
+        
     </div>
   );
 }
